873edfbc11112a01e415f4941052e47031e9cb22
Elly Ambet <ellyambet10@gmail.com>
2023-07-23 00:46:36 +0300

    modified model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { maximum: 30 }
+  validates :full_name, presence: true
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :phone_number, presence: true, uniqueness: true, length: { maximum: 100 }
+
+  has_secure_password
+  before_save { email.downcase! }
+
+  default_scope {order('users.created_at ASC')}
+
+  Roles = [:super_admin , :admin, :customer]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
 end
