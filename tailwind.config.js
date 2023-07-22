5e0a16ff9e9fee885fa68da3548086463b0549c0
Elly Ambet <ellyambet10@gmail.com>
2023-07-23 01:47:25 +0300

    Added preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   content: [
+    'node_modules/preline/dist/*.js',
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
