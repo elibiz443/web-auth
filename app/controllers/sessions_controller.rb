f6365bed144c8534fb8a88a0dceee3cf9d631621
Elly Ambet <ellyambet10@gmail.com>
2023-07-24 06:55:35 +0300

    Updated notification message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     begin
       @auth_user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
-      
+
       unless @auth_user
         @auth_user = User.new(
           provider: auth["provider"],
@@ -32,14 +32,18 @@ class SessionsController < ApplicationController
           password_digest: "#{Time.now.strftime('%d/%m/%Y-%H:%M:%S')}-#{rand.to_s[2..10]}",
           google_signup: true 
         )
-        
-        @auth_user.save(validate: false) 
+
+        unless @auth_user.save
+          flash[:alert] = @auth_user.errors.full_messages.join(', ')
+          redirect_to '/login'
+          return
+        end
       end
 
       session[:user_id] = @auth_user.id
-      redirect_to '/', notice: "Logged in as #{auth_user.first_name} #{auth_user.last_name} 👍"
+      redirect_to '/', notice: "Welcome 🎉 Logged in as #{@auth_user.first_name} #{@auth_user.last_name} 👍"
     rescue StandardError
-      flash[:alert] = @auth_user.errors.full_messages.join(', ')
+      flash[:alert] = 'Error creating user account.'
       redirect_to '/login'
     end
   end
